771bd4cf563cd70b43b901d7c36a972ab6085e83
joaquimserafim <joaquimserafim@users.noreply.github.com>
2024-03-27 15:49:04 +0000

    small tweak

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const isPalindrome = (str: string) => {
 	const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
 	const len = cleanStr.length;
-	const middle = len / 2;
+	const middle = (len / 2) << 0;
 
 	for (let i = 0; i < middle; i++) {
 		if (cleanStr[i] !== cleanStr[len - 1 - i]) {
